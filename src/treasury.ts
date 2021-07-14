dbc00a6d897f483c19ea89c0247187167a3f97fd
top1st <riguang102@gmail.com>
2021-07-15 02:58:57 +0800

    Update treasury.ts

--- src/treasury.ts
+++ src/treasury.ts
@@ -8,14 +8,13 @@ import {
 import {DaiTransfer, Reward} from "../generated/schema";
 import {loadRewardSummery, loadUser} from "./helpers";
 
-const tresuryAddress = '0x47eb130179cd0c25f11da3476f2493b5a0eb7a6b'
 
 export function handleTransfer(event: Transfer): void {
-    if(event.params.to.toHex() === tresuryAddress
+    if(event.params.to.toHex() === '0x47eb130179cd0c25f11da3476f2493b5a0eb7a6b'
         // && event.transaction.to.toHex() === '0xd4d2abbef1b26458504e7027233d5e7f09ea476d'
     ) {
         let reward = new Reward(event.transaction.hash.toHex() + '-' + event.logIndex.toString())
-        let user = loadUser(tresuryAddress)
+        let user = loadUser(event.params.to.toHex())
         reward.user = user.id
         reward.amount = event.params.value.toBigDecimal() / BigDecimal.fromString('1e18')
         user.totalReward = user.totalReward + reward.amount
