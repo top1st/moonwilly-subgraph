dc2a1f3e4ed84eb2f9e960c7061ae0e834ae3dee
top1st <riguang102@gmail.com>
2021-07-14 20:22:24 +0800

    remove exampleentity

--- src/tracker.ts
+++ src/tracker.ts
@@ -10,31 +10,31 @@ import {
     OwnershipTransferred,
     Transfer
 } from "../generated/TokenDividendTracker/TokenDividendTracker"
-import {ExampleEntity, Reward} from "../generated/schema"
+import {Reward} from "../generated/schema"
 
 export function handleApproval(event: Approval): void {
     // Entities can be loaded from the store using a string ID; this ID
     // needs to be unique across all entities of the same type
-    let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-    // Entities only exist after they have been saved to the store;
-    // `null` checks allow to create entities on demand
-    if (entity == null) {
-        entity = new ExampleEntity(event.transaction.from.toHex())
-
-        // Entity fields can be set using simple assignments
-        entity.count = BigInt.fromI32(0)
-    }
-
-    // BigInt and BigDecimal math are supported
-    entity.count = entity.count + BigInt.fromI32(1)
-
-    // Entity fields can be set based on event parameters
-    entity.owner = event.params.owner
-    entity.spender = event.params.spender
-
-    // Entities can be written to the store with `.save()`
-    entity.save()
+    // let entity = ExampleEntity.load(event.transaction.from.toHex())
+    //
+    // // Entities only exist after they have been saved to the store;
+    // // `null` checks allow to create entities on demand
+    // if (entity == null) {
+    //     entity = new ExampleEntity(event.transaction.from.toHex())
+    //
+    //     // Entity fields can be set using simple assignments
+    //     entity.count = BigInt.fromI32(0)
+    // }
+    //
+    // // BigInt and BigDecimal math are supported
+    // entity.count = entity.count + BigInt.fromI32(1)
+    //
+    // // Entity fields can be set based on event parameters
+    // entity.owner = event.params.owner
+    // entity.spender = event.params.spender
+    //
+    // // Entities can be written to the store with `.save()`
+    // entity.save()
 
     // Note: If a handler doesn't require existing field values, it is faster
     // _not_ to load the entity from the store. Instead, create it fresh with
@@ -89,7 +89,7 @@ export function handleClaimWaitUpdated(event: ClaimWaitUpdated): void {
 }
 
 export function handleDividendWithdrawn(event: DividendWithdrawn): void {
-    const reward = new Reward(`${event.transaction.hash}-${event.logIndex}`)
+    const reward = new Reward(event.transaction.hash + '-' + event.logIndex)
     reward.user = event.params.to
     reward.amount = event.params.weiAmount
     reward.save()
