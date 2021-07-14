d02cc10be96b7915cc767e98d3f440c777f1f99b
top1st <riguang102@gmail.com>
2021-07-14 22:21:52 +0800

    Update helpers.ts

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,17 +1,17 @@
 import { BigDecimal } from "@graphprotocol/graph-ts";
 import {RewardSummery, User} from "../generated/schema";
 
-function loadUser(id: string) {
+function loadUser(id: string): User {
     let user = User.load(id)
     if (!user) {
         user = new User(id)
         user.totalReward = 0;
         user.save()
     }
-    return user
+    return user as any
 }
 
-function loadRewardSummery() {
+function loadRewardSummery(): RewardSummery {
     let rewardSummery = RewardSummery.load("1")
     if(!rewardSummery) {
         rewardSummery = new RewardSummery("1")
@@ -19,7 +19,7 @@ function loadRewardSummery() {
         rewardSummery.totalReward = new BigDecimal(0)
         rewardSummery.save()
     }
-    return rewardSummery
+    return rewardSummery as any
 }
 
 export {
