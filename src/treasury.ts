802a23b4d65cb71ea6316cf027a61d3230740cd1
top1st <riguang102@gmail.com>
2021-07-15 02:40:32 +0800

    Update treasury.ts

--- src/treasury.ts
+++ src/treasury.ts
@@ -11,7 +11,9 @@ import {loadRewardSummery, loadUser} from "./helpers";
 const tresuryAddress = '0x47eb130179cd0c25f11da3476f2493b5a0eb7a6b'
 
 export function handleTransfer(event: Transfer): void {
-    if(event.params.to.toHex() === tresuryAddress && event.transaction.to.toHex() === '0xd4d2abbef1b26458504e7027233d5e7f09ea476d') {
+    if(event.params.to.toHex() === tresuryAddress
+        // && event.transaction.to.toHex() === '0xd4d2abbef1b26458504e7027233d5e7f09ea476d'
+    ) {
         let reward = new Reward(event.transaction.hash.toHex() + '-' + event.logIndex.toString())
         let user = loadUser(tresuryAddress)
         reward.user = user.id
