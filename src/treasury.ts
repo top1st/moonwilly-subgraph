7dbf5c5fed148fc77d0f587b2612aa62b18c0ddc
top1st <riguang102@gmail.com>
2021-07-17 01:05:58 +0800

    Update treasury.ts

--- src/treasury.ts
+++ src/treasury.ts
@@ -13,20 +13,20 @@ export function handleTransfer(event: Transfer): void {
     if(event.params.to.toHex() == '0x47eb130179cd0c25f11da3476f2493b5a0eb7a6b'
         && event.params.from.toHex() == '0xc7c3ccce4fa25700fd5574da7e200ae28bbd36a3'
     ) {
-        let reward = new Reward(event.transaction.hash.toHex() + '-' + event.logIndex.toString())
-        let user = loadUser(event.params.to.toHex())
-        reward.user = user.id
-        reward.amount = event.params.value.toBigDecimal() / BigDecimal.fromString('1e18')
-        user.totalReward = user.totalReward + reward.amount
-        user.save()
-        reward.blockNumber = event.block.number
-        reward.txHash = event.transaction.hash
-        reward.save()
+        // let reward = new Reward(event.transaction.hash.toHex() + '-' + event.logIndex.toString())
+        // let user = loadUser(event.params.to.toHex())
+        // reward.user = user.id
+        // reward.amount = event.params.value.toBigDecimal() / BigDecimal.fromString('1e18')
+        // user.totalReward = user.totalReward + reward.amount
+        // user.save()
+        // reward.blockNumber = event.block.number
+        // reward.txHash = event.transaction.hash
+        // reward.save()
+        let amount = event.params.value.toBigDecimal() / BigDecimal.fromString('1e18')
         let rewardSummery = loadRewardSummery()
-        rewardSummery.treasuryReward = rewardSummery.treasuryReward + reward.amount
-        rewardSummery.totalReward = rewardSummery.totalReward + reward.amount
+        rewardSummery.treasuryReward = rewardSummery.treasuryReward + amount
+        rewardSummery.totalReward = rewardSummery.totalReward + amount
         rewardSummery.totalCount++
         rewardSummery.save()
     }
-
 }
